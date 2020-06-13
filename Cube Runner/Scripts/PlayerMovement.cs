751b1d8eba42c348e353a8e1a98ca23be8774aca
hussy53 <56520422+hussy53@users.noreply.github.com>
2020-06-13 17:30:10 -0400

    Written in C#

---
+++ Cube Runner/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour   
+{
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        rb.AddForce(0, 200, 500);
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, sidewaysForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up, ForceMode.Impulse);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        else if(Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+        {
+
+        }
+    }
+}
